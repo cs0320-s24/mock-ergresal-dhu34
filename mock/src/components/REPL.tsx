341ecab12d2e1ccb99a83dffe3518d029344253c
Daniel Hu <daniel_hu4@brown.edu>
2024-03-01 01:10:20 -0500

    fix: patched syntax error in REPL.tsx

--- mock/src/components/REPL.tsx
+++ mock/src/components/REPL.tsx
@@ -11,16 +11,27 @@ import { REPLInput } from "./REPLInput";
   This is a great top level component for the REPL. It's a good idea to have organize all components in a component folder.
   You don't need to do that for this gearup.
 */
-
 export default function REPL() {
   const [history, setHistory] = useState<string[][]>([]);
-  const [output, setOutput] = useState<string>("");
+  const [mode, setMode] = useState<boolean>(false);
+  const [myData, setMyData] = useState<any[][]>([[]]);
 
+  /*
+  This is the highest level component for the REPL. It contains the REPL history and 
+  its inputs and initializes them in this function
+  */
   return (
     <div className="repl">
       <REPLHistory history={history} />
       <hr></hr>
-      <REPLInput history={history} setHistory={setHistory} mode={false} />
+      <REPLInput
+        history={history}
+        setHistory={setHistory}
+        mode={false}
+        setMode={setMode}
+        myData={[]}
+        setMyData={setMyData}
+      />
     </div>
   );
 }
